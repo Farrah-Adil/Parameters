cc7c0c6e04fd34985f9bf2410bd3469d73ebf065
Farah Adil <fadil@cedrus.com>
2023-07-20 13:13:24 +0500

    Commit Feat:Parameters Program Solved

---
+++ Parameters/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SumOfDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            string inputNumber = Console.ReadLine();
+
+            int sum = AddDigits(inputNumber);
+
+            Console.WriteLine($"Sum of digits: {sum}");
+        }
+
+        static int AddDigits(string numberText)
+        {
+            int sum = 0;
+
+            foreach (char digitChar in numberText)
+            {
+                if (char.IsDigit(digitChar))
+                {
+                    int digitValue = int.Parse(digitChar.ToString());
+                    sum += digitValue;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
